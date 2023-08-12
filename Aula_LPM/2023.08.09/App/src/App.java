f8fef9a89e858978c450bf0c5f57f87385d71bf0
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2023-08-12 07:57:53 -0300

    App ToDoSimple

---
+++ Aula_LPM/2023.08.09/App/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        final int TAMANHO = 6;
+        int[] inteiros;
+
+        inteiros = lerVetor(TAMANHO);
+        inteiros = inverter(inteiros);
+        System.out.println("\nInvertido");
+        imprimir(inteiros);
+        System.out.println("\nFIM");
+        
+    }
+
+    private static int[] lerVetor(int tAMANHO) {
+        return null;
+    }
+}
---
+++ Java Web Services with Spring Boot/todosimple/src/test/java/com/jhonatadias/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatadias.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
